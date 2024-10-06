ab3d70b34e2f3cfddb2534a6d4ee828581d9220b
Loumunix <olivier@bru-barth.com>
2024-10-06 08:54:27 +0200

    village

--- bla/Level/Décor/Village/maison 2/maison_2.gd
+++ bla/Level/Décor/Village/maison 2/maison_2.gd
@@ -1,9 +1,6 @@
 extends StaticBody2D
 
 
-
-
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		Globals.UI.get_node("press e").show()
---
+++ bla/UI/ui.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+
+func _ready() -> void:
+	Globals.UI = self
