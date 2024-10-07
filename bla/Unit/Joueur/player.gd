aad8f855f2ee6c363868deffc48bac5b2c8f7b25
Senjia12 <senjiacorp@gmail.com>
2024-10-07 22:08:35 +0200

    player mvts de base zqsd

--- bla/Unit/Joueur/player.gd
+++ bla/Unit/Joueur/player.gd
@@ -3,3 +3,12 @@ extends CharacterBody2D
 
 func _ready() -> void:
 	Globals.player = self
+
+func _physics_process(delta):
+	
+	velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	
+	velocity.y = Input.get_action_strength("bottom") - Input.get_action_strength("top")
+	velocity *= 150
+	
+	move_and_slide()
