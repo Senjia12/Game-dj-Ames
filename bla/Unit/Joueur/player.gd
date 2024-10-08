f5e964003ec7dc87685a3180106aaca32883edc3
Senjia12 <senjiacorp@gmail.com>
2024-10-08 22:34:27 +0200

    mvts player et animations

--- bla/Unit/Joueur/player.gd
+++ bla/Unit/Joueur/player.gd
@@ -5,10 +5,18 @@ func _ready() -> void:
 	Globals.player = self
 
 func _physics_process(delta):
-	
+	var idle_player = $"idle player"
 	velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	
 	velocity.y = Input.get_action_strength("bottom") - Input.get_action_strength("top")
-	velocity *= 150
+	velocity *= 50
 	
+	if velocity.length() > 0:
+		idle_player.play("walking player")
+		idle_player.speed_scale = 0.7
+		if velocity.x != 0:
+			idle_player.flip_h = velocity.x > 0
+			
+	else: 
+		idle_player.play("default")
+		idle_player.speed_scale = 0.3
 	move_and_slide()
