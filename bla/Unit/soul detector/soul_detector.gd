a236b1fa158a13778a03f655945d254b22c7d483
Loumunix <olivier@bru-barth.com>
2024-10-09 18:57:30 +0200

    épée + time stop + river

--- bla/Unit/soul detector/soul_detector.gd
+++ bla/Unit/soul detector/soul_detector.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var area_2d: Area2D = $Area2D
 
-
+var can_freeze := true
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("scan"):
@@ -14,13 +14,23 @@ func _physics_process(delta: float) -> void:
 			if i.is_in_group("soul"):
 				i.activate()
 	
-#	if Input.is_action_just_pressed("timestop"):
-#		if SceneTree.paused:
-#			SceneTree.paused = false
-#		else:
-#			SceneTree.paused = true
+	if Input.is_action_just_pressed("time stop") && can_freeze:
+		get_tree().paused = true
+		$"freeze duration".start()
+		$"freeze cd".start()
+		$CanvasLayer/grey.show()
+		can_freeze = false
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("soul"):
 		body.desactivate()
+
+
+func _on_freeze_duration_timeout() -> void:
+	get_tree().paused = false
+	$CanvasLayer/grey.hide()
+
+
+func _on_freeze_cd_timeout() -> void:
+	can_freeze = true
